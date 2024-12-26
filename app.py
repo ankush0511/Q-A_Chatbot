fe128e29ba64159fe06e452426015081811a5a52
ankush0511 <136951570+ankush0511@users.noreply.github.com>
2024-12-26 04:28:46 +0000

    languageTranslation

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from google.protobuf import message
 import streamlit as st
 from langchain_community.llms import ollama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.messages import HumanMessage
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
@@ -29,19 +32,26 @@ def generate_response(question, max_tokens, temperature):
   answer = chain.invoke({'question': question})
   return answer
 
+prompt=ChatPromptTemplate.from_messages(
+    [("system","you are a helpful assistant. answer all the question to the nest of your ability in {Language}"),
+     MessagesPlaceholder(variable_name="messages")
+     ])
+chain=prompt|model
+
+lanres=chain.invoke({"messages":[HumanMessage(content='{response}')],
+   "Language":"{language}"})
 
 st.title("Q&A Chatbot")
 st.sidebar.title("Settings")
 # llm=st.title("gemmabot")
 temperature=st.sidebar.slider("temperature",min_value=0.0,max_value=1.0,value=0.7)
 max_tokens=st.sidebar.slider("Max Tokens",min_value=50,max_value=300,value=100)
-
-
+language=st.sidebar.selectbox("Select Language",("English","Spanish","German","French","Italian","Hindi"))
 st.write("Go ahead")
 
 user_inputs=st.text_input("Ask a question")
 if user_inputs:
-  response=generate_response(user_inputs,max_tokens,temperature)
-  st.write(response)
+  response=chain.invoke({"messages":[HumanMessage(content=user_inputs)],"Language":language})
+  st.write(response.content)
 else:
   st.write("Please ask a question")
